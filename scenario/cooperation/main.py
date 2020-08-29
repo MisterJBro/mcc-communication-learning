a3b9433d8731e6bf488c0dc98a5c83662489e558
Julien B <MisterJBro@protonmail.com>
2020-08-29 12:08:31 +0200

    First simple protocol

--- scenario/cooperation/main.py
+++ scenario/cooperation/main.py
@@ -19,7 +19,7 @@ PROJECT_PATH = pathlib.Path(
 
 class Agents:
     def __init__(self, seed=0, device='cuda:0', lr_collector=1e-3, lr_guide=1e-3, gamma=0.99, max_steps=500,
-                 fc_hidden=64, rnn_hidden=128, batch_size=256, iters=20, lam=0.97, clip_ratio=0.2, target_kl=0.03,
+                 fc_hidden=64, rnn_hidden=128, batch_size=512, iters=20, lam=0.97, clip_ratio=0.2, target_kl=0.02,
                  num_layers=1, grad_clip=1.0, symbol_num=5, tau=1.0):
         # RNG seed
         random.seed(seed)
