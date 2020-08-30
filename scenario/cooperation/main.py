cd683af472649007202a281645347af9d7a00e2a
Julien B <MisterJBro@protonmail.com>
2020-08-30 19:17:37 +0200

    bug fixes

--- scenario/cooperation/main.py
+++ scenario/cooperation/main.py
@@ -18,7 +18,7 @@ PROJECT_PATH = pathlib.Path(
 
 
 class Agents:
-    def __init__(self, seed=0, device='cuda:0', lr_collector=6.25e-4, lr_guide=6.25e-4, gamma=0.99, max_steps=500,
+    def __init__(self, seed=0, device='cuda:0', lr_collector=1e-3, lr_guide=1e-3, gamma=0.99, max_steps=500,
                  fc_hidden=64, rnn_hidden=128, batch_size=128, iters=40, lam=0.97, clip_ratio=0.2, target_kl=0.03,
                  num_layers=1, grad_clip=1.0, symbol_num=5, tau=1.0):
         # RNG seed
@@ -100,7 +100,7 @@ class Agents:
             batch_rew += team_rew
 
             obs = next_obs
-            #msg = next_msg
+            msg = next_msg
         self.reward_and_advantage()
         self.reset_states()
 
@@ -139,7 +139,7 @@ class Agents:
             obs[1], self.state_g)
 
         act_c = act_dist_c.sample().cpu().numpy()
-        act_g = np.ones(self.batch_size)*4  # act_dist_g.sample().cpu().numpy()
+        act_g = act_dist_g.sample().cpu().numpy()
 
         return np.stack([act_c, act_g]).T, next_msg
 
@@ -152,67 +152,62 @@ class Agents:
         kl_approx = (old_logp - logp).mean().item()
         return loss, kl_approx
 
-    def update_net(self, net, opt, obs, act, adv, ret, old_logp, msg=None, other_net=None, other_obs=None):
+    def update_net(self, net, opt, obs, act, adv, ret, msg=None, other_net=None, other_obs=None):
+        policy_loss = 0
+        value_loss = 0
+        with torch.no_grad():
+            if msg is not None:
+                old_logp = net.action_only(
+                    obs, msg).log_prob(act).to(self.device)
+            else:
+                old_logp = net.action_only(
+                    obs).log_prob(act).to(self.device)
 
-        if msg is not None:
-            dist, vals = net(obs, msg)
-        else:
-            dist, _, vals = net(obs)
+        for i in range(self.iters):
+            self.optimizer_c.zero_grad()
+            self.optimizer_g.zero_grad()
 
-        loss, kl = self.compute_policy_gradient(
-            net, dist, act, adv, old_logp)
-        grad_loss = loss.item()
-        if kl > self.target_kl:
-            return 0, 0, True
-        loss.backward(retain_graph=True)
+            if msg is not None:
+                dist, vals = net(obs, msg)
+            else:
+                dist, _, vals = net(obs)
 
-        loss = self.criterion(vals.reshape(-1), ret)
-        val_loss = loss.item()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(
-            net.parameters(), self.grad_clip)
+            loss, kl = self.compute_policy_gradient(
+                net, dist, act, adv, old_logp)
+            policy_loss += loss.item()
+            if kl > self.target_kl:
+                return policy_loss, value_loss
+            loss.backward(retain_graph=True)
 
-        return grad_loss, val_loss, False
+            loss = self.criterion(vals.reshape(-1), ret)
+            value_loss += loss.item()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(
+                net.parameters(), self.grad_clip)
+
+            self.optimizer_c.step()
+            self.optimizer_g.step()
+            if other_net is not None:
+                _, msg, _ = other_net(other_obs[:, :-1])
+                msg = msg.reshape(self.batch_size, self.max_steps-1, -1)
+                msg = torch.cat(
+                    (torch.zeros(self.batch_size, 1, self.symbol_num).to(self.device), msg), 1)
+
+        return policy_loss, value_loss
 
     def update(self):
         obs_c, act_c, ret_c, adv_c, msg, obs_g, act_g, ret_g, adv_g = self.buffers.get_tensors(
             self.device)
 
-        total_pol_loss_c, total_val_loss_c = 0, 0
+        p_loss_c, v_loss_c = self.update_net(
+            self.collector, self.optimizer_c, obs_c, act_c, adv_c, ret_c, msg=msg, other_net=self.guide, other_obs=obs_g)
+        p_loss_g, v_loss_g = self.update_net(
+            self.guide, self.optimizer_g, obs_g, act_g, adv_g, ret_g)
 
-        with torch.no_grad():
-            old_logp_c = self.collector.action_only(
-                obs_c, msg).log_prob(act_c).to(self.device)
-            old_logp_g = self.guide.action_only(
-                obs_g).log_prob(act_g).to(self.device)
-
-        for i in range(self.iters):
-            self.optimizer_c.zero_grad()
-            # self.optimizer_g.zero_grad()
-
-            pol_loss_c, val_loss_c, done_c = self.update_net(
-                self.collector, self.optimizer_c, obs_c, act_c, adv_c, ret_c, old_logp_c, msg=msg)
-            # loss_g, done_g = self.update_net(
-            #    self.guide, self.optimizer_g, obs_g, act_g, adv_g, ret_g, old_logp_g)
-            done_g = False
-
-            total_pol_loss_c += pol_loss_c
-            total_val_loss_c += val_loss_c
-
-            self.optimizer_c.step()
-            # self.optimizer_g.step()
-
-            if done_c or done_g:
-                break
-
-            #_, msg, _ = self.guide(obs_g[:, :-1])
-            #msg = msg.reshape(self.batch_size, self.max_steps-1, -1)
-            # msg = torch.cat(
-            #    (torch.zeros(self.batch_size, 1, self.symbol_num).to(self.device), msg), 1)
         msg_ent = Categorical(
             probs=msg.reshape(-1, self.symbol_num).detach().cpu().mean(0)).entropy().item()
 
-        return total_pol_loss_c, total_val_loss_c, msg_ent
+        return p_loss_c, v_loss_c, p_loss_g, v_loss_g, msg_ent
 
     def train(self, epochs):
         """ Trains the agent for given epochs """
@@ -225,7 +220,7 @@ class Agents:
             if rew > self.max_rew:
                 self.max_rew = rew
                 self.save()
-            p_loss_c, v_loss_c, msg_ent = self.update()
+            p_loss_c, v_loss_c, p_loss_g, v_loss_g, msg_ent = self.update()
 
             print('Epoch: {:4}  Average Reward: {:5}  Collector - Policy Loss {:4}  Value Loss {:4}  Message Entropy: {:4}'.format(
                 epoch, np.round(rew, 3), np.round(p_loss_c, 3), np.round(v_loss_c, 3), np.round(msg_ent, 3)))
@@ -267,7 +262,7 @@ class Agents:
             #msg[0] = torch.tensor([0., 0., 0., 1., 0.]).to(self.device)
             self.envs.envs[0].render()
             print(msg[0].detach().cpu().numpy())
-            acts, next_msg = self.get_actions(obs, msg)
+            acts, msg = self.get_actions(obs, msg)
             obs, rews, _, _ = self.envs.step(acts)
             obs = self.preprocess(obs)
 
