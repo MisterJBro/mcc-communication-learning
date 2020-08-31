d622f7f9d53265d79f4b698768676cda5a39d844
Julien B <MisterJBro@protonmail.com>
2020-09-01 01:43:07 +0200

    better parameters

--- scenario/cooperation/main.py
+++ scenario/cooperation/main.py
@@ -19,7 +19,7 @@ PROJECT_PATH = pathlib.Path(
 
 class Agents:
     def __init__(self, seed=0, device='cuda:0', lr_collector=1e-3, lr_guide=1e-3, gamma=0.99, max_steps=500,
-                 fc_hidden=64, rnn_hidden=128, batch_size=128, iters=40, lam=0.97, clip_ratio=0.2, target_kl=0.03,
+                 fc_hidden=64, rnn_hidden=128, batch_size=256, iters=40, lam=0.97, clip_ratio=0.2, target_kl=0.03,
                  num_layers=1, grad_clip=1.0, symbol_num=5, tau=1.0, entropy_factor=-0.1):
         # RNG seed
         random.seed(seed)
@@ -295,10 +295,9 @@ class Agents:
             msg_sum += msg[0].detach().cpu().numpy()
             acts, msg = self.get_actions(obs, msg)
             obs, rews, _, _ = self.envs.step(acts)
-            print(rews[0][1])
             obs = self.preprocess(obs)
 
-            episode_rew += rews[0][0] + rews[0][1]
+            episode_rew += rews[0][0]
         print('Result reward: ', episode_rew)
         print(msg_sum)
         self.reset_states()
