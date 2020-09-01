755d290fbb2367bc9176abec409acd6e25a6bfdb
Julien B <MisterJBro@protonmail.com>
2020-09-01 18:10:10 +0200

    Reached 12 Average reward

--- scenario/cooperation/main.py
+++ scenario/cooperation/main.py
@@ -18,7 +18,7 @@ PROJECT_PATH = pathlib.Path(
 
 
 class Agents:
-    def __init__(self, seed=0, device='cuda:0', lr_collector=1e-4, lr_guide=1e-4, gamma=0.99, max_steps=500,
+    def __init__(self, seed=0, device='cuda:0', lr_collector=1e-7, lr_guide=1e-7, gamma=0.99, max_steps=500,
                  fc_hidden=64, rnn_hidden=128, batch_size=256, iters=40, lam=0.97, clip_ratio=0.2, target_kl=0.03,
                  num_layers=1, grad_clip=1.0, symbol_num=5, tau=1.0, entropy_factor=-0.1):
         # RNG seed
@@ -231,9 +231,9 @@ class Agents:
         _, _ = self.update_net(
             self.collector, self.optimizer_c, obs_c, act_c, adv_c, ret_c, 5, msg=msg, other_net=self.guide, other_obs=obs_g, other_opt=self.optimizer_g, other_act=act_g, other_adv=adv_g, other_ret=ret_g)
         p_loss_c, v_loss_c = self.update_net(
-            self.collector, self.optimizer_c, obs_c, act_c, adv_c, ret_c, 30, msg=msg.detach())
+            self.collector, self.optimizer_c, obs_c, act_c, adv_c, ret_c, 0, msg=msg.detach())
         p_loss_g, v_loss_g = self.update_net(
-            self.guide, self.optimizer_g, obs_g, act_g, adv_g, ret_g, 30)
+            self.guide, self.optimizer_g, obs_g, act_g, adv_g, ret_g, 0)
 
         return p_loss_c, v_loss_c, p_loss_g, v_loss_g, msg_ent
 
@@ -323,7 +323,7 @@ class Agents:
 if __name__ == "__main__":
     agents = Agents()
     agents.load()
-    # agents.train(2000)
+    agents.train(2000)
 
     import code
     # code.interact(local=locals())
