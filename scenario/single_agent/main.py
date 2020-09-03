5f372d6984bf5d38c6320b478c3c2f9b37220c01
Julien B <MisterJBro@protonmail.com>
2020-09-03 15:54:08 +0200

    c

--- scenario/single_agent/main.py
+++ scenario/single_agent/main.py
@@ -18,8 +18,8 @@ PROJECT_PATH = pathlib.Path(
 
 
 class Agents:
-    def __init__(self, seed=0, device='cuda:0', lr_collector=1e-3, lr_guide=1e-3, gamma=0.99, max_steps=500,
-                 fc_hidden=64, rnn_hidden=128, batch_size=128, iters=40, lam=0.97, clip_ratio=0.2, target_kl=0.03,
+    def __init__(self, seed=0, device='cuda:0', lr_collector=1e-4, lr_guide=1e-4, gamma=0.99, max_steps=500,
+                 fc_hidden=64, rnn_hidden=128, batch_size=1024, iters=40, lam=0.97, clip_ratio=0.2, target_kl=0.03,
                  num_layers=1, grad_clip=1.0, symbol_num=5, tau=1.0):
         # RNG seed
         random.seed(seed)
@@ -231,8 +231,7 @@ class Agents:
 
 if __name__ == "__main__":
     agents = Agents()
-    agents.max_rew = 0
-    # agents.load()
+    agents.load()
     agents.train(200)
 
     while True:
