129ac3dd172e03d4f811c8c89fda8f1c2e4bc0c9
Julien B <MisterJBro@protonmail.com>
2020-09-25 11:18:41 +0200

    c

--- scenario/mcc/main.py
+++ scenario/mcc/main.py
@@ -24,7 +24,7 @@ RED_GUIDE_ID = 2
 
 
 class Agents:
-    def __init__(self, seed=0, device='cuda:0', lr_collector=5e-4, lr_guide=5e-4, lr_enemy=5e-4, lr_critic=1e-3, gamma=0.99, max_steps=500,
+    def __init__(self, seed=0, device='cuda:0', lr_collector=2e-4, lr_guide=2e-4, lr_enemy=2e-4, lr_critic=4e-4, gamma=0.99, max_steps=500,
                  fc_hidden=64, rnn_hidden=128, batch_size=256, iters=40, lam=0.97, td_lam=0.97, clip_ratio=0.2, target_kl=0.01, critic_iters=1,
                  num_layers=1, grad_clip=1.0, symbol_num=5, tau=1.0):
         # RNG seed
@@ -550,7 +550,7 @@ class Agents:
 
 if __name__ == "__main__":
     agents = Agents()
-    # agents.load()
+    agents.load()
     agents.train(1000)
 
     import code
