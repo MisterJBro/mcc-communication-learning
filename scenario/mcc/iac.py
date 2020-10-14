b9156fe2ccb71d5df207b6ae31beb8ead12eb521
Julien B <MisterJBro@protonmail.com>
2020-10-14 13:39:17 +0200

    c

--- scenario/mcc/iac.py
+++ scenario/mcc/iac.py
@@ -55,11 +55,11 @@ class Agents:
             self.guide.parameters(), lr=lr_guide)
         self.optimizer_e = optim.Adam(
             self.enemy.parameters(), lr=lr_enemy)
-        milestones = [80, 4000, 5000]
+        milestones = [80, 180, 5000]
         self.scheduler_c = MultiStepLR(
             self.optimizer_c, milestones=milestones, gamma=0.2)
         self.scheduler_g = MultiStepLR(
-            self.optimizer_g, milestones=milestones, gamma=0.1)
+            self.optimizer_g, milestones=milestones, gamma=0.2)
         self.scheduler_e = MultiStepLR(
             self.optimizer_e, milestones=milestones, gamma=0.2)
         self.batch_size = batch_size
@@ -288,7 +288,7 @@ class Agents:
         p_loss_g, v_loss_g = self.update_net(
             self.guide, self.optimizer_g, obs_g, act_g, adv_g, ret_g, 0)
         p_loss_e, v_loss_e = self.update_net(
-            self.enemy, self.optimizer_e, obs_e, act_e, adv_e, ret_e, 20, enemy=True)
+            self.enemy, self.optimizer_e, obs_e, act_e, adv_e, ret_e, 10, enemy=True)
 
         if self.red_iters > 0:
             self.scheduler_c.step()
@@ -362,7 +362,7 @@ class Agents:
             obs = self.preprocess(obs)
 
             episode_rew += rews[0][0]
-            print(rews[0])
+            # print(rews[0])
         print('Result reward: ', episode_rew)
         print(msg_sum)
         self.reset_states()
@@ -392,7 +392,7 @@ class Agents:
 if __name__ == "__main__":
     agents = Agents()
     # agents.load()
-    agents.train(500)
+    agents.train(200)
 
     import code
     # code.interact(local=locals())
